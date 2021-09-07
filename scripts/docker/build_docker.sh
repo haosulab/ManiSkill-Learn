092eb9701beb2521ed8a128f7169f9ff8e8d51d2
lz1oceani <z6ling@ucsd.edu>
2021-09-06 23:18:41 -0700

    Add docker file

---
+++ scripts/docker/build_docker.sh
@@ -0,0 +1,4 @@
+docker build . < Dockerfile -t maniskill-2021
+# docker rm -f  maniskill # Delete the container
+docker run --runtime=nvidia -itd -v /tmp/.X11-unix:/tmp/.X11-unix --rm --name maniskill maniskill-2021:latest /bin/bash -c "while true; do echo hello world; sleep 1; done"
+docker exec -it maniskill bash
